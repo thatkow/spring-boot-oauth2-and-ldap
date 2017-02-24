13a5bb91751c71b9d2076bdbec7dc9eca608540a
Andrew Kowalczyk <andrew@diversityarrarys.com>
2017-02-24 20:04:36 +1100

    Added readme and tweaked controller to add user's email to display

---
+++ README.md
@@ -0,0 +1,6 @@
+# Usage
+
+```
+gradle build
+java -jar build/libs/authenticating-oauth2-0.1.0.jar
+```
--- src/main/java/com/example/Controllers.java
+++ src/main/java/com/example/Controllers.java
@@ -1,16 +1,29 @@
 package com.example;
 
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Controllers {
+
+	@SuppressWarnings("unchecked")
 	@RequestMapping("/")
-	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
-			Model model) {
-		model.addAttribute("name", name);
+	public String query(Principal principal, Model model) {
+		if (principal != null) {
+			OAuth2Authentication oAuth2Authentication = (OAuth2Authentication) principal;
+			Authentication authentication = oAuth2Authentication.getUserAuthentication();
+			Map<String, String> details = new HashMap<String, String>();
+			details = (Map<String, String>) authentication.getDetails();
+			if (details.containsKey("email"))
+				model.addAttribute("email", details.get("email"));
+		}
 		return "portal";
 	}
 }
